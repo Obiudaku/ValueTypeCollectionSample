1337603ffa5a1fc2b7357c0ec851b5839576ec6b
user <user@DESKTOP-VP4U2A2>
2024-04-23 21:10:18 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ValueTypeCollectionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ValueTypeCollection<int> intCollection = new ValueTypeCollection<int>();
+            intCollection.AddItem(5);
+            intCollection.AddItem(3);
+            intCollection.AddItem(8);
+
+            Console.WriteLine("Items:");
+            foreach (var item in intCollection.GetSortedDescending())
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Item at index 1: " + intCollection.GetItem(1));
+        }
+    }
+}
---
+++ ValueTypeCollectionExample.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ValueTypeCollection<T> where T : struct
+{
+    private List<T> items;
+
+    public ValueTypeCollection()
+    {
+        items = new List<T>();
+    }
+
+    public void AddItem(T item)
+    {
+        items.Add(item);
+    }
+
+    public T GetItem(int index)
+    {
+        if (index < 0 || index >= items.Count)
+        {
+            throw new IndexOutOfRangeException("Index is out of range");
+        }
+
+        return items[index];
+    }
+
+    public List<T> GetSortedDescending()
+    {
+        return items.OrderByDescending(i => i).ToList();
+    }
+}
